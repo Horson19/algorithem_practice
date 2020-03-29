165d356dd35886f1094e52c453a1ff33aaea78a6
Horson <Horson.c19@gmail.com>
2020-03-29 21:39:39 +0800

    ac

---
+++ AC/AC/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AC
+//
+//  Created by HorsonChan on 2020/3/29.
+//  Copyright © 2020 Horson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
